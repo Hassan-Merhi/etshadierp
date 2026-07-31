7172fcb36e173f4b074f3307bdaa5330b35895a2
Hassan Dakik <hassmerhi.etshadi@gmail.com>
2026-07-31 08:06:51 -0500

    Use the canonical registered read microcache

--- server/routes.ts
+++ server/routes.ts
@@ -1,12 +1,10 @@
 import type { Express } from "express";
 import type { Server } from "http";
 
-import { installPrivateApiCache } from "./middleware/privateApiCache";
 import { registerApplicationRoutes } from "./routes/applicationRoutes";
 import { registerOperationalMonitoringRoutes } from "./routes/admin/operationalMonitoringRoutes";
 
 export function registerRoutes(app: Express): Promise<Server> {
-  installPrivateApiCache(app);
   registerOperationalMonitoringRoutes(app);
   return registerApplicationRoutes(app);
 }
