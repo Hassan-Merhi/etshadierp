da73e389c13dee3453e01083161a140395101b92
Hassan Dakik <hassmerhi.etshadi@gmail.com>
2026-08-03 19:42:52 +0200

    Phase 4: add reusable scrollable dialog body

--- client/src/components/ui/dialog.tsx
+++ client/src/components/ui/dialog.tsx
@@ -81,7 +81,7 @@ const DialogContent = React.forwardRef<
         onKeyDown={handleKeyDown}
         onCloseAutoFocus={handleCloseAutoFocus}
         className={cn(
-          "fixed left-1/2 top-1/2 z-50 grid max-h-[calc(100dvh-1rem)] w-[calc(100vw-1rem)] max-w-lg -translate-x-1/2 -translate-y-1/2 gap-4 overflow-y-auto overscroll-contain border bg-background p-4 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 sm:max-h-[calc(100dvh-2rem)] sm:w-[calc(100vw-2rem)] sm:rounded-lg sm:p-6 motion-reduce:animate-none motion-reduce:transition-none",
+          "fixed left-1/2 top-1/2 z-50 grid max-h-[calc(var(--app-viewport-height)-1rem)] w-[calc(100vw-1rem)] max-w-lg -translate-x-1/2 -translate-y-1/2 gap-4 overflow-y-auto overscroll-contain border bg-background p-4 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 sm:max-h-[calc(var(--app-viewport-height)-2rem)] sm:w-[calc(100vw-2rem)] sm:rounded-lg sm:p-6 motion-reduce:animate-none motion-reduce:transition-none",
           className,
         )}
         {...props}
@@ -105,10 +105,19 @@ const DialogHeader = ({ className, ...props }: React.HTMLAttributes<HTMLDivEleme
 );
 DialogHeader.displayName = "DialogHeader";
 
+const DialogBody = ({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) => (
+  <div
+    className={cn("min-h-0 min-w-0 overflow-y-auto overscroll-contain", className)}
+    data-dialog-body="true"
+    {...props}
+  />
+);
+DialogBody.displayName = "DialogBody";
+
 const DialogFooter = ({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) => (
   <div
     className={cn(
-      "flex flex-col-reverse gap-2 border-t pt-4 sm:flex-row sm:justify-end sm:border-t-0 sm:pt-0 [&>*]:w-full sm:[&>*]:w-auto",
+      "flex flex-col-reverse gap-2 border-t pt-4 sm:flex-row sm:justify-end sm:border-t-0 sm:pt-0 [&>*]:min-h-11 [&>*]:w-full sm:[&>*]:w-auto",
       className,
     )}
     {...props}
@@ -148,6 +157,7 @@ export {
   DialogTrigger,
   DialogContent,
   DialogHeader,
+  DialogBody,
   DialogFooter,
   DialogTitle,
   DialogDescription,
