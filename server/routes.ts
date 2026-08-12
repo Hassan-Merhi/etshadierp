28a3a871392cb0a31172efc41d04686f020a250a
Hassan Dakik <hassmerhi.etshadi@gmail.com>
2026-08-13 00:10:56 +0200

    security: put tenant boundary before every API registrar

--- server/routes.ts
+++ server/routes.ts
@@ -6,8 +6,8 @@ import { registerApplicationRoutes } from "./routes/applicationRoutes";
 import { registerOperationalMonitoringRoutes } from "./routes/admin/operationalMonitoringRoutes";
 
 export function registerRoutes(app: Express): Promise<Server> {
-  registerOperationalMonitoringRoutes(app);
   app.use(tenantIsolationBoundary);
   app.param("companyId", tenantCompanyParamBoundary);
+  registerOperationalMonitoringRoutes(app);
   return registerApplicationRoutes(app);
 }
